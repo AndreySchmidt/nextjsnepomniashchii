037b285c89d2966961980959f2f4b013b700ebcc
AndreySchmidt <ringnull@gmail.com>
2023-05-25 16:30:05 +0300

    working with a state manager zustand

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,28 +1,15 @@
-"use client";
-
 import { PostSearch } from "@/components/PostSearch";
 import { Posts } from "@/components/Posts";
-import { getAllPosts } from "@/services/getPosts";
 import { Metadata } from "next";
-import { useEffect, useState } from "react";
 
 export const metadata: Metadata = { title: "Blog" };
 
 export default function Blog() {
-  const [posts, setPosts] = useState<any[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    getAllPosts()
-      .then(setPosts)
-      .finally(() => setLoading(false));
-  }, []);
-
   return (
     <>
       <h1>Blog</h1>
-      <PostSearch onSearch={setPosts} />
-      {loading ? <h3>Loading...</h3> : <Posts posts={posts} />}
+      <PostSearch />
+      <Posts />
     </>
   );
 }
