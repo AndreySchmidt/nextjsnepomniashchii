57532ac9d5394bed15e65d032b6273684195d68e
AndreySchmidt <ringnull@gmail.com>
2023-05-25 10:47:16 +0300

    loading error files

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,6 +5,8 @@ async function getData() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
     next: { revalidate: 60 },
   });
+
+  if (!response.ok) throw new Error("Unable to fetch posts");
   return response.json();
 }
 
